8364438e63ed5317f1b2537436f372b86d559fb0
TarkovGit <kingtwst@gmail.com>
2024-08-03 22:56:34 +0200

    ID

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,7 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// Fonction pour récupérer l'ID du photographe à partir de l'URL
+export async function getPhotographerIdFromURL() {
+  const currentURL = window.location.href;
+  const url = new URL(currentURL);
+  const params = new URLSearchParams(url.search);
+  return params.get("id");
+}
